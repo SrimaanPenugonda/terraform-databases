9ed1f7cd0ea787f03cdb088269c44e1fd0773f8f
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-05-10 00:11:41 +0530

    changed name

--- rds.tf
+++ rds.tf
@@ -1,38 +1,38 @@
-resource "aws_db_subnet_group" "mysql" {
-  name       = "mysql"
+resource "aws_db_subnet_group" "mysqldb" {
+  name       = "mysqldb"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
   tags       = {
     name     = "Mysql DB subnet grp"
   }
 }
 
-resource "aws_rds_cluster_parameter_group" "mysql" {
+resource "aws_rds_cluster_parameter_group" "mysqldb" {
   name   = "mysql-cluster-pg-${var.ENV}"
   family = "aurora-mysql5.7"
   description = "RDS default cluster parameter group"
 }
 
-resource "aws_rds_cluster" "mysql" {
+resource "aws_rds_cluster" "mysqldb" {
   cluster_identifier              = "mysql-${var.ENV}"
   engine                          = "aurora-mysql"
   engine_version                  = "5.7.mysql_aurora.2.03.2"
-  db_subnet_group_name            = aws_db_subnet_group.mysql.name
+  db_subnet_group_name            = aws_db_subnet_group.mysqldb.name
   database_name                   = "defaultdb"
   master_username                 = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_USER"]
   master_password                 = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_PASS"]
   backup_retention_period         = 5
   preferred_backup_window         = "07:00-09:00"
-  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.mysql.name
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.mysqldb.name
   vpc_security_group_ids          = [aws_security_group.allow_mysql.id]
 }
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
   count              = 1
   identifier         = "mysql-${var.ENV}-${count.index}"
-  cluster_identifier = aws_rds_cluster.mysql.id
+  cluster_identifier = aws_rds_cluster.mysqldb.id
   instance_class     = "db.t3.small"
-  engine             = aws_rds_cluster.mysql.engine
-  engine_version     = aws_rds_cluster.mysql.engine_version
+  engine             = aws_rds_cluster.mysqldb.engine
+  engine_version     = aws_rds_cluster.mysqldb.engine_version
 }
 
 
