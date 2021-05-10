97d40123e0e2378302a13f19b65e7401f717ac34
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-05-10 10:07:47 +0530

    mysqldb to mysql

--- rds.tf
+++ rds.tf
@@ -1,44 +1,44 @@
-resource "aws_db_subnet_group" "mysqldb" {
-  name       = "mysqldb"
+resource "aws_db_subnet_group" "mysql" {
+  name       = "mysql"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
   tags       = {
     name     = "Mysql DB subnet grp"
   }
 }
 
-resource "aws_rds_cluster_parameter_group" "mysqldb" {
-  name   = "mysqldb-cluster-pg-${var.ENV}"
+resource "aws_rds_cluster_parameter_group" "mysql" {
+  name   = "mysql-cluster-pg-${var.ENV}"
   family = "aurora-mysql5.7"
   description = "RDS default cluster parameter group"
 }
 
-resource "aws_rds_cluster" "mysqldb" {
-  cluster_identifier              = "mysqldb-${var.ENV}"
+resource "aws_rds_cluster" "mysql" {
+  cluster_identifier              = "mysql-${var.ENV}"
   engine                          = "aurora-mysql"
   engine_version                  = "5.7.mysql_aurora.2.03.2"
-  db_subnet_group_name            = aws_db_subnet_group.mysqldb.name
+  db_subnet_group_name            = aws_db_subnet_group.mysql.name
   database_name                   = "defaultdb"
   master_username                 = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_USER"]
   master_password                 = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_PASS"]
   backup_retention_period         = 5
   preferred_backup_window         = "07:00-09:00"
-  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.mysqldb.name
-  vpc_security_group_ids          = [aws_security_group.allow_mysqldb.id]
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.mysql.name
+  vpc_security_group_ids          = [aws_security_group.allow_mysql.id]
 }
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
   count              = 1
-  identifier         = "mysqldb-${var.ENV}-${count.index}"
-  cluster_identifier = aws_rds_cluster.mysqldb.id
+  identifier         = "mysql-${var.ENV}-${count.index}"
+  cluster_identifier = aws_rds_cluster.mysql.id
   instance_class     = "db.t3.small"
-  engine             = aws_rds_cluster.mysqldb.engine
-  engine_version     = aws_rds_cluster.mysqldb.engine_version
+  engine             = aws_rds_cluster.mysql.engine
+  engine_version     = aws_rds_cluster.mysql.engine_version
 }
 
 
-resource "aws_security_group" "allow_mysqldb" {
-  name        = "allow-mysqldb-${var.ENV}"
-  description = "allow-mysqldb-${var.ENV}"
+resource "aws_security_group" "allow_mysql" {
+  name        = "allow-mysql-${var.ENV}"
+  description = "allow-mysql-${var.ENV}"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
   ingress {
     description = "SSH"
@@ -56,7 +56,7 @@ resource "aws_security_group" "allow_mysqldb" {
   }
 
   tags = {
-    Name = "allow-mysqldb-${var.ENV}"
+    Name = "allow-mysql-${var.ENV}"
   }
 }
 
@@ -65,7 +65,7 @@ resource "null_resource" "mysql-schema" {
     command = <<EOF
     git clone https://github.com/SrimaanPenugonda/rs-mysql.git
     cd rs-mysql
-    mysql -h ${aws_rds_cluster.mysqldb.endpoint} -u ${jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_USER"]} -p${jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_PASS"]} <shipping.sql
+    mysql -h ${aws_rds_cluster.mysql.endpoint} -u ${jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_USER"]} -p${jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_PASS"]} <shipping.sql
 EOF
   }
 }
